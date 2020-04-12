c3f67ba473743f0e1e8cc27f60ad075f2aec3743
Neha Kumari <nehak.23j@gmail.com>
2020-04-12 11:40:13 +0530

    changed up

---
+++ codenew.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number\n");
+    scanf("%d\n",&a);
+    printf("the 15th multiple of the number is %d",a*15);
+    printf("byee");
+    return 0;
+}
